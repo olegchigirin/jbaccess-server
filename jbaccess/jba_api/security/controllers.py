879bb2d3e9111069800aba5331f08b7829fe1c65
dmitriy.selischev <dmitriy.selischev@logicify.com>
2017-06-29 20:51:13 +0300

    Fixed login action

--- jbaccess/jba_api/security/controllers.py
+++ jbaccess/jba_api/security/controllers.py
@@ -4,17 +4,16 @@ from django.http import HttpRequest
 
 from jba_api import errors
 from jba_api import permissions
-from jba_api.common import JbAccessController
+from jba_api.common import JbAccessController, dto_inject
 from jba_api.security.dto import LoginInDto, UserInfoOutDto
 from jba_core.service import UserService
 
 
 class LoginController(JbAccessController):
-    def post(self, request: HttpRequest):
-        dto = LoginInDto.from_dict(request.data)
-        if not dto.is_valid():
-            return ApiResponse.bad_request(dto)
+    @dto_inject(LoginInDto)
+    def post(self, request: HttpRequest, dto: LoginInDto):
         user = UserService.get_user_by_credentials(dto.login, dto.password)
+        auth.login(request, user)
         if user is None:
             return ApiResponse.not_found(errors.INCORRECT_CREDENTIALS)
         return ApiResponse.success(UserInfoOutDto.from_user(user))
