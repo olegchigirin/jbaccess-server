d663a182e62046df9c7eabecf69fce53ed8a2d93
dmitriy.selischev <dmitriy.selischev@logicify.com>
2017-07-26 15:11:56 +0300

    Renamed static_root directory

--- jbaccess/jba_server/settings/base.py
+++ jbaccess/jba_server/settings/base.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'skj435h5lrkfsj;r34prosafdlkmc;alskfl905hok6jfIs'
 DEBUG = False
 ALLOWED_HOSTS = []
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
