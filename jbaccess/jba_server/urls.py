663dfcd88f36851e1234031684c9b1b531b6c447
dmitriy.selischev <dmitriy.selischev@logicify.com>
2017-07-26 14:47:12 +0300

    Introduce django admin site urls

--- jbaccess/jba_server/urls.py
+++ jbaccess/jba_server/urls.py
@@ -1,8 +1,10 @@
 import api_commons.common
 from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('jba_api.urls')),
+    url(r'^admin/?', admin.site.urls),
 ]
 
 handler404 = api_commons.common.error_404_handler
